6226f1cc1f4652cd105f3a77b69eddde26bd7e18
yusribjb <yusribjb@gmail.com>
2020-04-24 16:18:43 +0800

    Initial commit

---
+++ app/src/main/java/com/android/praktikum/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.android.praktikum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView1 = findViewById<TextView>(R.id.textView1);
+        textView1.setOnClickListener(View.OnClickListener {
+            Toast.makeText(applicationContext: this, text: "hai",Toast.LENGTH_SHORT).show()
+        })             
+
+
+    }
+}
